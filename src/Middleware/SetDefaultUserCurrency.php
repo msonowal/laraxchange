bbfac5e214b29eb926f60bffc4869d9fc6c867cb
Manash Jyoti Sonowal <manash.sonowal@conversionbug.com>
2017-05-30 21:22:48 +0530

    updated with logging

--- src/Middleware/SetDefaultUserCurrency.php
+++ src/Middleware/SetDefaultUserCurrency.php
@@ -3,18 +3,21 @@
 namespace Msonowal\Laraxchange\Middleware;
 
 use Closure;
+use Illuminate\Support\Facades\Log;
 
 class SetDefaultUserCurrency
 {
     public function handle($request, Closure $next)
     {
         $location   =   geoip()->getLocation();
+        Log::info($location);
         $currency   =   $location->currency;
 
         if ( is_null($currency) || $currency=='' ) {
             $currency = config('currency.default_currency');
         }
         setDefaultCurrencyIfNotSet($currency);
+        Log::info($currency);
         return $next($request);
     }
 }
