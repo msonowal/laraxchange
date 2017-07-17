afd96affd29943133c4c24cf2d0c2a4d4c9e26a9
Manash Jyoti Sonowal <manash.sonowal@conversionbug.com>
2017-07-17 10:47:02 +0530

    removed logging

--- src/Middleware/SetDefaultUserCurrency.php
+++ src/Middleware/SetDefaultUserCurrency.php
@@ -13,16 +13,16 @@ class SetDefaultUserCurrency
             //$location   =   geoip()->getLocation(); //Commented cause it was giving inconsistent results
             $location   =   geoip($request->ip());
             $currency   =   $location->currency;
-            Log::debug('Detected Location ', $location->toArray());
+            //Log::debug('Detected Location ', $location->toArray());
         } catch (\Exception $ex) {
             Log::debug('Exception caught '. $ex->getMessage());
         }
         if (is_null($currency) || $currency=='') {
             $currency = config('currency.default_currency');
         }
-        Log::debug('Setting Currency if not Set previously :'. $currency);
+        //Log::debug('Setting Currency if not Set previously :'. $currency);
         setDefaultCurrencyIfNotSet($currency);
-        Log::debug('Loading currency of Session IP :'.$request->ip(). ' Currency: '. $currency);
+        //Log::debug('Loading currency of Session IP :'.$request->ip(). ' Currency: '. $currency);
         return $next($request);
     }
 }
